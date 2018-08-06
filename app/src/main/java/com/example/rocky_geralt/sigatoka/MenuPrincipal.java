3aad1d7eb52881e9026737a87f2d5103c337d7a0
Geralt <rockygeralt@gmail.com>
2018-08-06 12:42:07 -0500

    Cambio de menu principal, botones en el menu para acceder a los diferentes layouts con sus respectivas imagenes

--- app/src/main/java/com/example/rocky_geralt/sigatoka/MenuPrincipal.java
+++ app/src/main/java/com/example/rocky_geralt/sigatoka/MenuPrincipal.java
@@ -13,9 +13,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MenuPrincipal extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private Button btn_sigatoka;
+    private Button btn_estadios;
+    private Button btn_prevenir;
+    private Button btn_sennova;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +30,42 @@ public class MenuPrincipal extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        btn_sigatoka = findViewById(R.id.btn_sigatoka);
+        btn_estadios = findViewById(R.id.btn_estadios);
+        btn_prevenir = findViewById(R.id.btn_prevenir);
+        btn_sennova = findViewById(R.id.btn_sennova);
+
+        btn_sigatoka.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuPrincipal.this, Sigatoka.class);
+                startActivity(intent);
+                MenuPrincipal.this.finish();
+            }
+        });
+
+        btn_estadios.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuPrincipal.this, Estadios.class);
+                startActivity(intent);
+                MenuPrincipal.this.finish();
+            }
+        });
+        btn_prevenir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuPrincipal.this, Prevenir.class);
+                startActivity(intent);
+                MenuPrincipal.this.finish();
+            }
+        });
+        btn_sennova.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuPrincipal.this, Sennova.class);
+                startActivity(intent);
+                MenuPrincipal.this.finish();
             }
         });
 
@@ -66,11 +102,6 @@ public class MenuPrincipal extends AppCompatActivity
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
