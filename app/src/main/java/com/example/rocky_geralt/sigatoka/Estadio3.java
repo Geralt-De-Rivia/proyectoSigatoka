6a5999d66fbb7ddc3f105cb5fe7af0653eb6ca07
Geralt <rockygeralt@gmail.com>
2018-08-13 17:50:24 -0500

    Cambio en el layout del Estadio 3, se creo la clase del SliderAdapter, se colocaron las imagenes del estadio 3 en Slider

--- app/src/main/java/com/example/rocky_geralt/sigatoka/Estadio3.java
+++ app/src/main/java/com/example/rocky_geralt/sigatoka/Estadio3.java
@@ -1,13 +1,22 @@
 package com.example.rocky_geralt.sigatoka;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Estadio3 extends AppCompatActivity {
 
+    private ViewPager viewPager;
+    private SliderAdapter miAdaptador;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_estadio3);
+        viewPager = findViewById(R.id.viewPager);
+        miAdaptador = new SliderAdapter(this);
+        viewPager.setAdapter(miAdaptador);
+
+
     }
 }
---
+++ app/src/main/java/com/example/rocky_geralt/sigatoka/SliderAdapter.java
@@ -0,0 +1,93 @@
+package com.example.rocky_geralt.sigatoka;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class SliderAdapter extends PagerAdapter{
+    Context context;
+    LayoutInflater inflater;
+
+    //Lista de imagenes
+    public int[] lista_imagenes = {
+            R.drawable.estadio3,
+            R.drawable.estadio3_1,
+            R.drawable.estadio3_2,
+            R.drawable.estadio3_3,
+            R.drawable.estadio3_4,
+            R.drawable.estadio3_5
+    };
+
+    //Lista de titulos
+    public String[] lista_titulos ={
+            "Estadio 3",
+            "Estadio 3.1",
+            "Estadio 3.2",
+            "Estadio 3.3",
+            "Estadio 3.4",
+            "Estadio 3.5"
+    };
+
+    //Lista de descripcion
+
+    public String[] lista_descripcion = {
+            "Descipcion 1",
+            "Descipcion 2",
+            "Descipcion 3",
+            "Descipcion 4",
+            "Descipcion 5",
+            "Descipcion 6"
+    };
+
+    //Opcional Lista de color de fondo
+    public int[] lista_color_fondo = {
+            Color.rgb(55,55,55),
+            Color.rgb(239,55,55),
+            Color.rgb(110,49,89),
+            Color.rgb(1,188,212),
+            Color.rgb(1,85,85),
+            Color.rgb(110,55,55),
+    };
+
+    public SliderAdapter(Context context){
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return lista_titulos.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return (view ==(LinearLayout)object);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        inflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.slider, container, false);
+        LinearLayout layoutSlide = view.findViewById(R.id.slideLinearLayout);
+        ImageView imagenSlide = view.findViewById(R.id.slideImagen);
+        TextView txtTitulo = view.findViewById(R.id.txtTitulo);
+        TextView txtDescripcion = view.findViewById(R.id.txtDescripcion);
+        layoutSlide.setBackgroundColor(lista_color_fondo[position]);
+        imagenSlide.setImageResource(lista_imagenes[position]);
+        txtTitulo.setText(lista_titulos[position]);
+        txtDescripcion.setText(lista_descripcion[position]);
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((LinearLayout) object);
+    }
+}
